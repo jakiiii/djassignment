853c3a0cb949c9da7435362c809f0e3c57822c22
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2022-11-27 15:20:55 +0600

    update serializer

--- product/serializers.py
+++ product/serializers.py
@@ -3,16 +3,17 @@ from rest_framework import serializers
 from product.models import Product, ProductImage
 
 
-class ProductImageSerializer(serializers.ModelSerializer):
+class ProductImageListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductImage
-        fields = ('id', 'product', 'image')
+        fields = ('image',)
 
 
 class ProductListSerializer(serializers.HyperlinkedModelSerializer):
     posted_by = serializers.StringRelatedField(read_only=True)
     category = serializers.StringRelatedField(read_only=True)
+    product_image = ProductImageListSerializer(many=True)
 
     class Meta:
         model = Product
@@ -27,6 +28,7 @@ class ProductListSerializer(serializers.HyperlinkedModelSerializer):
             'description',
             'review',
             'is_feature',
+            'product_image',
         )
         extra_kwargs = {
             'url': {'view_name': 'product:product_detail', 'lookup_field': 'uid'},
